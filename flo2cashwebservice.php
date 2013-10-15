d1c6bfb58cae63ab9c3584b985b39f20dbb7f187
Chris Burgess <chris@giantrobot.co.nz>
2013-10-16 12:05:41 +1300

    Code style.

--- flo2cashwebservice.php
+++ flo2cashwebservice.php
@@ -3,6 +3,8 @@
 /**
  * @file
  *
+ * Flo2Cash WebService payment processor for CiviCRM.
+ *
  * @package CRM
  * @copyright Giant Robot Ltd 2007-2012
  */
@@ -43,13 +45,16 @@ require_once 'CRM/Core/Payment.php';
 class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
 
   /**
-   * mode of operation: live or test
+   * Mode of operation: live or test.
    *
    * @var object
    * @static
    */
   static protected $_mode = NULL;
 
+  /**
+   * Singleton pattern.
+   */
   static private $_singleton = NULL;
 
   /**
@@ -94,16 +99,11 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
         }
       }
       catch (CiviCRM_API3_Exception $e) {
-        $errorMessage = $e->getMessage();
-        $errorCode = $e->getErrorCode();
-        $errorData = $e->getExtraParams();
+        $error_message = $e->getMessage();
+        $error_code = $e->getErrorCode();
+        $error_data = $e->getExtraParams();
       }
     }
-
-    // Other settings here.
-    // $this->_setParam('timestamp', time());
-    // srand(time());
-    // $this->_setParam('sequence', rand(1, 1000));
   }
 
   /**
@@ -122,37 +122,38 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
     }
 
     if (empty($this->_paymentProcessor['signature'])) {
-        $error[] = ts('Account ID is not set in Administer CiviCRM &raquo; Configure &raquo; Global Settings &raquo; Payment Processors &raquo; ' . $this->_paymentProcessor['name']);
+      $error[] = ts('Account ID is not set in Administer CiviCRM &raquo; Configure &raquo; Global Settings &raquo; Payment Processors &raquo; ' . $this->_paymentProcessor['name']);
     }
 
     if (empty($this->_paymentProcessor['password'])) {
-        $error[] = ts('Password is not set in Administer CiviCRM &raquo; Configure &raquo; Global Settings &raquo; Payment Processors &raquo; ' . $this->_paymentProcessor['name']);
+      $error[] = ts('Password is not set in Administer CiviCRM &raquo; Configure &raquo; Global Settings &raquo; Payment Processors &raquo; ' . $this->_paymentProcessor['name']);
     }
 
     if (empty($this->_paymentProcessor['url_api'])) {
       $error[] = ts('API URL is not set in Administer CiviCRM &raquo; Configure &raquo; Global Settings &raquo; Payment Processors &raquo; ' . $this->_paymentProcessor['name']);
     }
 
-    //self::debug($this->_paymentProcessor, '$this->_paymentProcessor');
-
     if (! empty($error)) {
       return implode('<p>', $error);
-    } else {
+    }
+    else {
       return NULL;
     }
   }
 
   /**
-   * Do direct payment against Flo2Cash WSDL
+   * Do direct payment against Flo2Cash WSDL.
    *
-   * @param array $params  name value pair of contribution datat
+   * @param array $params
+   *   name value pair of contribution datat
    *
-   * @return array $params or CiviCRM error array
+   * @return array
+   *   $params or CiviCRM error array
+   *
    * @access public
-   *
    */
   function doDirectPayment(&$params) {
-    if (! defined('CURLOPT_SSLCERT')) {
+    if (!defined('CURLOPT_SSLCERT')) {
       return self::error(9001, 'Flo2Cash WebService requires curl with SSL support');
     }
 
@@ -163,27 +164,36 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
     // Ensure amount is in F2C expected format.
     $soap_vars['Amount'] = sprintf("%01.2f", $soap_vars['Amount']);
 
-    $reference = substr($this->_getParam('last_name') .', '. $this->_getParam('first_name') .' - '. $this->_getParam('email'), 0, 50);
+    $reference = $this->_getParam('last_name') .
+      ', ' . $this->_getParam('first_name') .
+      ' - ' . $this->_getParam('email');
+    $reference = substr($reference, 0, 50);
 
     // Alter credit card type to F2C expected format.
     switch ($this->_getParam('credit_card_type')) {
       case 'Visa':
         $card_type = 'VISA';
-        break ;
+        break;
+
       case 'MasterCard':
         $card_type = 'MC';
         break;
+
       case 'Amex':
         $card_type = 'AMEX';
         break;
+
       case 'Diners Club':
       case 'Diners':
-        // Flo2Cash WebService offers Diners which CiviCRM doesn't offer by default.
-        // To enable this, visit
+        // Flo2Cash WebService offers Diners which CiviCRM doesn't
+        // offer by default.  To enable this, visit
+        //
         // civicrm/admin/options/accept_creditcard?group=accept_creditcard&reset=1
+        //
         // and add a card type of 'Diners Club'
         $card_type = 'DINERS';
         break;
+
       default:
         // CiviCRM offers "Discover" by default, which Flo2Cash WebService
         // doesn't support.  To disable this, visit the URL below and
@@ -195,22 +205,25 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
         // processing if F2C reject it.
         //
         // $card_type = strtoupper($this->_getParam['credit_card_type']);
-        return self::error(9004, 'Unsupported credit card type: '. $this->_getParam['credit_card_type']);
+        return self::error(9004, 'Unsupported credit card type: ' . $this->_getParam['credit_card_type']);
     }
 
     // Frequency ID - only used for recurring.
-    switch ($this->_getParam( 'frequency_unit' )) {
+    switch ($this->_getParam('frequency_unit')) {
       case 'week':
-        $freqId = 2;
+        $frequency_id = 2;
         break;
+
       case 'month':
-        $freqId = 7;
+        $frequency_id = 7;
         break;
+
       case 'year':
-        $freqId = 13;
+        $frequency_id = 13;
         break;
+
       default:
-        $freqId = 0;
+        $frequency_id = 0;
         break;
     }
 
@@ -220,23 +233,27 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
         'Username'       => $this->_paymentProcessor['user_name'],
         'Password'       => $this->_paymentProcessor['password'],
         'PlanDetails'    => array(
-          // 'CardName'              => '', // F2C don't seem to validate this anyway.
+           // F2C don't seem to validate this anyway?
+          // 'CardName'              => '',
           'CardName'             => $this->_getParam('first_name') . ' ' . $this->_getParam('last_name'),
           // 'CardName'             => $this->_getParam('credit_card_owner'),
           'CardNumber'            => $this->_getParam('credit_card_number'),
           'CardType'              => $card_type,
           'CardExpiry'            => str_pad($this->_getParam('month'), 2, '0', STR_PAD_LEFT) . $exp_year = substr($this->_getParam('year'), -2),
           'Amount'                => sprintf('%01.2f', $this->_getParam('amount')),
-          'CountryID'             => 112, // New Zealand – See Appendix B
-          'ClientId'              => $this->_paymentProcessor['user_name'], // Flo2Cash Client ID
-          'ClientAccountId'       => $this->_paymentProcessor['signature'], // Flo2Cash Account ID
-          'FrequencyId'           => $freqId, // See Appendix C
+          // New Zealand – See Appendix B.
+          'CountryID'             => 112,
+          'ClientId'              => $this->_paymentProcessor['user_name'],
+          'ClientAccountId'       => $this->_paymentProcessor['signature'],
+           // See Appendix C.
+          'FrequencyId'           => $frequency_id,
           // 'NumberOfPayment'      => 10,
           // 'terminationdate'      => '2008/12/25',
           'StartDate'             => strtotime('tomorrow'),
           'Reference'             => $reference,
           'Particular'            => $params['invoiceID'],
-          // 'Reference'            => $this->_getParam( 'credit_card_owner' ), // Merchant Particular
+          // Merchant Particular.
+          // 'Reference'            => $this->_getParam( 'credit_card_owner' ),
         ),
       );
       // dpm(var_export($soap_vars,1), 'vars');
@@ -248,12 +265,6 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
         'Password'      => $this->_paymentProcessor['password'],
         'AccountId'     => $this->_paymentProcessor['signature'],
         'Amount'        => sprintf('%01.2f', $this->_getParam('amount')),
-        /**
-         * I think this is a much nicer reference, but it's not
-         * unique. If you want to do this, do it in the CiviCRM alter
-         * payment processor params hook instead.
-         */
-        // 'Reference'    => substr(trim($this->_getParam('billing_first_name') .' '. $this->_getParam('billing_last_name') .' ('. $this->_getParam('email') .')'), 0, 50),
         'Reference'     => $reference,
         'Particular'    => $params['invoiceID'],
         'Email'         => $this->_getParam('email'),
@@ -267,58 +278,32 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
       );
     }
 
-    //dpm($soap_vars, 'vars');
     CRM_Utils_Hook::alterPaymentProcessorParams($this, $params, $soap_vars);
 
     try {
-      $PaymentService = new F2CSoapClient($this->_paymentProcessor['url_api'], array('trace' => 1));
-      $result = $PaymentService->$soap_method($soap_vars);
-      /*
-        self::debug(array(
-        '$PaymentService' => $PaymentService,
-        '$soap_vars' => $soap_vars,
-        '$result' => $result,
-        '$fault' => $fault,
-        '$this' => $this,
-        'url' => $this->_paymentProcessor['url_api'],
-        ), 'success');
-      */
+      $payment_service = new F2CSoapClient($this->_paymentProcessor['url_api'], array('trace' => 1));
+      $result = $payment_service->$soap_method($soap_vars);
     }
     catch (SoapFault $fault) {
-      /*
-        self::debug(array(
-        '$PaymentService' => $PaymentService,
-        '$soap_vars' => $soap_vars,
-        '$result' => $result,
-        '$fault' => $fault,
-        '$this' => $this,
-        'url' => $this->_paymentProcessor['url_api'],
-        ), 'failure');
-      */
       if (isset($fault->faultcode)) {
-        $Actor = $fault->faultcode;
+        $actor = $fault->faultcode;
       }
       if (isset($fault->detail->error->errortype)) {
-        $ErrorType = $fault->detail->error->errortype;
+        $error_type = $fault->detail->error->errortype;
       }
       if (isset($fault->detail->error->errornumber)) {
-        $ErrorNumber = $fault->detail->error->errornumber;
+        $error_number = $fault->detail->error->errornumber;
       }
       if (isset($fault->detail->error->errormessage)) {
-        $ErrorMessage = $fault->detail->error->errormessage;
+        $error_message = $fault->detail->error->errormessage;
       }
       elseif (isset($fault->faultstring)) {
-        $ErrorMessage = $fault->faultstring;
+        $error_message = $fault->faultstring;
       }
       else {
-        $ErrorMessage = 'Unknown SOAP error.';
+        $error_message = 'Unknown SOAP error.';
       }
-      /*
-        dpm($PaymentService, 'PaymentService');
-        dpm($fault, 'fault');
-        dpm($PaymentService->__getLastRequest(), 'lastRequest');
-      */
-      return self::error(9003, $ErrorMessage);
+      return self::error(9003, $error_message);
     }
     catch (Exception $ex) {
       return self::error(9002, $ex->get_errmsg());
@@ -326,34 +311,43 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
   }
 
   /**
-   * Set a field to the specified value.  Value must be a scalar (int,
-   * float, string, or boolean)
+   * Set a field to the specified (scalar) value.
    *
    * @param string $field
+   *   Field to set value of.
+   *
    * @param mixed $value
-   * @return bool false if value is not a scalar, true if successful
+   *   Value to set in $field.
+   *
+   * @return bool
+   *   FALSE if value is not a scalar, TRUE if successful.
    */
   function _setParam($field, $value) {
-    if (! is_scalar($value)) {
-      return false;
-    } else {
+    if (!is_scalar($value)) {
+      return FALSE;
+    }
+    else {
       $this->_params[$field] = $value;
     }
   }
 
   /**
-   * Get the value of a field if set
+   * Get the value of a field if set.
    *
-   * @param string $field the field
-   * @return mixed value of the field, or empty string if the field is
-   * not set
+   * @param string $field
+   *   The field to obtain a value from.
+   *
+   * @return mixed
+   *   Value of the field, or empty string if the field is
+   *   not set
    */
   function _getParam($field) {
     return CRM_Utils_Array::value($field, $this->_params, '');
   }
 
   /**
-   * This payment processor is a direct payment processor, so we do not transfer.
+   * This payment processor is a direct payment processor, so we do
+   * not transfer.
    */
   function doTransferCheckout(&$params, $component) {
     CRM_Core_Error::fatal(ts('This function is not implemented'));
@@ -362,11 +356,12 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
   /**
    * @TODO Document this function.
    */
-  function &error($errorCode = NULL, $errorMessage = NULL) {
+  function &error($error_code = NULL, $error_message = NULL) {
     $e =& CRM_Core_Error::singleton();
-    if ($errorCode) {
-      $e->push($errorCode, 0, NULL, $errorMessage);
-    } else {
+    if ($error_code) {
+      $e->push($error_code, 0, NULL, $error_message);
+    }
+    else {
       $e->push(9001, 0, NULL, 'Unknown System Error.');
     }
     return $e;
@@ -377,7 +372,8 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
    */
   function debug($debug, $label = '') {
     if (function_exists('watchdog')) {
-      watchdog('civicrm', '!label: <pre>!debug</pre>', array('!label' => $label, '!debug' => htmlspecialchars(print_r($debug,1))), WATCHDOG_DEBUG);
+      $wargs = array('!label' => $label, '!debug' => htmlspecialchars(print_r($debug, 1)));
+      watchdog('civicrm', '!label: <pre>!debug</pre>', $wargs, WATCHDOG_DEBUG);
     }
     if (function_exists('dpm')) {
       dpm($debug, $label);
@@ -385,17 +381,15 @@ class nz_co_fuzion_Flo2CashWebService extends CRM_Core_Payment {
   }
 }
 
+/**
+ * Wrapper for SOAP class.
+ */
 class F2CSoapClient extends SoapClient {
+
+  /**
+   * Implements SOAP::__doRequest().
+   */
   public function __doRequest($request, $location, $action, $version, $one_way = FALSE) {
-    /*
-      nz_co_fuzion_Flo2CashWebService::debug(array(
-          'request' => $request,
-          'location' => $location,
-          'action' => $action,
-          'version' => $version,
-          'one_way' => $one_way,
-         ), 'SOAP request');
-    */
     $response = parent::__doRequest($request, $location, $action, $version, $one_way);
     // nz_co_fuzion_Flo2CashWebService::debug($response, 'response');
     if (!$one_way) {
